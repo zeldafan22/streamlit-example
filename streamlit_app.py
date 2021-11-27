31a4d2513315577ee7888e276e72332f2b4c81d3
zeldafan22 <riperhior@gmail.com>
2021-11-28 00:47:53 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
-import matplotlib.pyplot as plt
+
 
 st.title('Patrones de consumo en Almería (2015)')
 
