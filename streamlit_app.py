9f4359c0654cb58113c6a10815fa8a3cbbb8a2da
zeldafan22 <riperhior@gmail.com>
2021-11-28 00:42:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,17 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Patrones de consumo en Almería (201')
+st.title('Patrones de consumo en Almería (2015)')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://c0a2-35-204-66-47.ngrok.io/api/v1/sectores')
 
 @st.cache
 def load_data():
-    data = pd.read_json(DATA_URL)
+    #data = pd.read_json(DATA_URL)
+
+    data ='''{"SECTOR":{"0":"HOGAR","1":"AUTO","2":"ALIMENTACION","3":"MODA Y COMPLEMENTOS","4":"BELLEZA","5":"RESTAURACION","6":"OTROS","7":"SALUD","8":"OCIO Y TIEMPO LIBRE","9":"TECNOLOGIA"},"avg(IMPORTE)":{"0":84.7371467942,"1":169.549617678,"2":152.4559131232,"3":132.5147521945,"4":68.6948310345,"5":56.2500331352,"6":74.0997393714,"7":115.3074300204,"8":75.5608778935,"9":134.9544598268}}'''
+    df = pd.read_json(data, orient ='index')
     '''
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
