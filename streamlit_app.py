3d07be7c6bceca545c98c5d6c3cabfd1f0b8d93f
zeldafan22 <riperhior@gmail.com>
2021-11-28 00:47:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.figure_factory as ff
+import matplotlib.pyplot as plt
 
 st.title('Patrones de consumo en Almería (2015)')
 
@@ -9,20 +11,20 @@ DATA_URL = ('https://c0a2-35-204-66-47.ngrok.io/api/v1/sectores')
 
 @st.cache
 def load_data():
-    #data = pd.read_json(DATA_URL)
-
-    data ='''{"SECTOR":{"0":"HOGAR","1":"AUTO","2":"ALIMENTACION","3":"MODA Y COMPLEMENTOS","4":"BELLEZA","5":"RESTAURACION","6":"OTROS","7":"SALUD","8":"OCIO Y TIEMPO LIBRE","9":"TECNOLOGIA"},"avg(IMPORTE)":{"0":84.7371467942,"1":169.549617678,"2":152.4559131232,"3":132.5147521945,"4":68.6948310345,"5":56.2500331352,"6":74.0997393714,"7":115.3074300204,"8":75.5608778935,"9":134.9544598268}}'''
-    df = pd.read_json(data, orient ='index')
+    data = pd.read_json(DATA_URL)
     '''
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
     data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
     return data
     '''
-    return df
-st.subheader('Gasto total por mes')
-data = load_data()
-st.bar_chart(data, width=0, height=0, use_container_width=True)
+    return data
+
+st.subheader('Gasto total por sector')
+data_sectores = load_data()
+st.write(data_sectores)
+#Bar Chart
+st.bar_chart(data_sectores, width=0, height=0, use_container_width=True)
 
 '''
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
