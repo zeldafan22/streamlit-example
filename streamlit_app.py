dd6b1a93a6c736c9e65b0ff9b1e8daf0da1cc545
zeldafan22 <riperhior@gmail.com>
2021-11-28 01:31:11 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ def recoger(url):
 def load_data():
     datos = recoger(DATA_URL)
     data = pd.DataFrame(datos)
-    data.drop(columns=data.columns[0])
+    data.drop(columns=0)
     
     '''
     lowercase = lambda x: str(x).lower()
