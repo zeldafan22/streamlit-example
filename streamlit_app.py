f66be777ae3a9dc5ade82ecc0bb1efb897022106
zeldafan22 <riperhior@gmail.com>
2021-11-28 01:40:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,4 +27,4 @@ st.subheader('Gasto total por sector')
 data_sectores = load_data()
 #st.write(data_sectores)
 #Bar Chart
-st.bar_chart(data_sectores[DATA_COLUMN])
+st.bar_chart(data_sectores[DATA_COLUMN], height=300)
