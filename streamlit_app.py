287a5293e6b64ed716037e0539dc04349fefc5e7
zeldafan22 <riperhior@gmail.com>
2021-11-28 19:07:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -87,7 +87,7 @@ data_talta = load_data_taltas()
 st.vega_lite_chart(data_talta, {
     'mark': 'bar',
     'encoding' : {
-        'x' : {'field': 'DIA', 'type': 'ordinal', 'timeUnit': 'day'},
+        'x' : {'field': 'DIA', 'type': 'temporal', 'timeUnit': 'day'},
         'y' : {'field': 'GASTOS', 'type': 'quantitative'},
         'color': {'field': 'TMed', 'type': 'quantitative'},
             
