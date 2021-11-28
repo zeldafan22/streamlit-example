c8f6e77a0cc6697f2931bd2c75a0df48f7aa2cd9
zeldafan22 <riperhior@gmail.com>
2021-11-28 01:33:50 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,13 +19,6 @@ def load_data():
     datos = recoger(DATA_URL)
     data = pd.DataFrame(datos)
     data = data.iloc[:, ::-1]
-    
-    '''
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
-    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-    return data
-    '''
     return data
 
 st.subheader('Gasto total por sector')
@@ -34,15 +27,3 @@ data_sectores = load_data()
 st.write(data_sectores)
 #Bar Chart
 st.bar_chart(data_sectores[DATA_COLUMN])
-
-'''
-hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-st.bar_chart(hist_values)
-
-# Some number in the range 0-23
-hour_to_filter = st.slider('hour', 0, 23, 17)
-filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-
-st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-st.map(filtered_data)
-'''
